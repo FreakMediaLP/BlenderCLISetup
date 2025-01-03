293e2b443b34ae9c0c18e50cdf30c1ed301c4fd9
Freak <131407675+FreakMediaLP@users.noreply.github.com>
2025-01-03 15:35:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 mkdir -p "$HOME/Blender"
 
 # Download aliasses and prepend it to .bashrc
-curl -c https://raw.githubusercontent.com/FreakMediaLP/Blender-CLI-Setup/main/aliases.txt | cat - ~/.bashrc > temp && mv temp ~/.bashrc
+curl -s https://raw.githubusercontent.com/FreakMediaLP/Blender-CLI-Setup/main/aliases.txt | cat - ~/.bashrc > temp && mv temp ~/.bashrc
 
 # Activate color prompt
 sed -i 's/^#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
